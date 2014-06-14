4e1d41421c651a287321f0b6ffde67eb94f586c1
Nathan Chere <git@nathanchere.com.au>
2014-06-14 22:44:02 +0100

    Basic REST client

--- nPlivo/Client.cs
+++ nPlivo/Client.cs
@@ -2,11 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using RestSharp;
 
 namespace nPlivo
 {
     public class Client
-    {
+    {       
+        private const string BASE_URL = "https://api.plivo.com";
+        private readonly RestClient restClient;
 
+        public Client(string authId, string authToken)
+        {           
+            restClient = new RestClient
+            {
+                Authenticator = new HttpBasicAuthenticator(authId, authToken),
+                UserAgent = "nPlivo",
+                BaseUrl = String.Format("{0}/v1/Account/{1}", BASE_URL, authId)
+            };
+        }
     }
+
+
 }
--- nPlivo/Properties/AssemblyInfo.cs
+++ nPlivo/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("nPlivo")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription(".Net client for Plivo REST API")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Nathan Chere")]
 [assembly: AssemblyProduct("nPlivo")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
