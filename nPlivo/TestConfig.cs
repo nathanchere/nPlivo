dcc66567baaba7da23717fefef73037a0b78ccf8
Nathan Chere <git@nathanchere.com.au>
2014-06-15 17:40:10 +0100

    Test config

---
+++ nPlivo/TestConfig.cs
@@ -0,0 +1,10 @@
+﻿namespace nPlivo
+{
+    public static class TestConfig
+    {
+        public const string AuthId = @"Put your auth ID here";
+        public const string AuthToken = @"Put your auth token here";
+        public const string PhoneNumberVirtual = @"Put your virtual/plivo phone number here";
+        public const string PhoneNumberReal = @"Put your real phone number here";   
+    }
+}
